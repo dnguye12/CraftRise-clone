f0c02519326f166daa7f8cab9e0e854e836ed56a
duc-huy.nguyen <duc-huy.nguyen@universite-paris-saclay.fr>
2022-09-04 08:37:26 +0200

    header section done

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let menuBar = document.getElementById("menu-bar");
+let headerMobile = document.getElementById("header-mobile");
 let menuDropdown = document.getElementById("menu-mobile-dropdown");
 
-menuBar.addEventListener("click", function () {
+headerMobile.addEventListener("click", function () {
     if (menuBar.src.match("../images/menu-bar.png")) {
         menuBar.src = "../images/menu-close.png";
         menuDropdown.style.visibility = "visible";
@@ -12,3 +13,29 @@ menuBar.addEventListener("click", function () {
         menuDropdown.style.opacity = "0";
     }
 });
+
+let playBtns = document.querySelectorAll(".playBtn");
+let playSection = document.getElementById("playSection");
+let playOverlay = document.getElementById("playOverlay");
+
+function playFunction() {
+    if(playSection.style.opacity == "0") {
+        playSection.style.opacity = "1";
+        playSection.style.visibility = "visible";
+        playOverlay.style.opacity = "1";
+        playOverlay.style.visibility = "visible";
+    }else {
+        playSection.style.opacity = "0";
+        playSection.style.visibility = "hidden";
+        playOverlay.style.opacity = "0";
+        playOverlay.style.visibility = "hidden";
+    }
+}
+
+playOverlay.addEventListener("click", function () {
+    playFunction();
+});
+
+playBtns.forEach(e => e.addEventListener("click", function() {
+    playFunction();
+}));
